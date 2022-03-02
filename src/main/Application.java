b0a13f7d1a818f78358569fb8e170972744553ea
Mic293 <mmichela2903@gmail.com>
2022-03-02 16:05:25 +0100

    Add mbase execution time, memory usage

--- src/main/Application.java
+++ src/main/Application.java
@@ -5,8 +5,21 @@ import java.io.FileNotFoundException;
 
 public class Application {
     public static void main(String[] args) throws FileNotFoundException {
-        var line = "C:\\Users\\mmich\\Desktop\\VS_workspace\\Algoritmi---Strutture-Dati---2020_2021\\src\\main\\resources\\74L85.000.matrix";
+        
+        var line = "C:\\Users\\mmich\\Desktop\\VS_workspace\\Algoritmi-2020-2021\\src\\main\\resources\\74L85.000.matrix";
+        
         var mbase = new MBase();
-        var res = mbase.mbase(MatrixParser.parse(line));
+        var matrix = MatrixParser.parse(line);
+
+        var runtime = Runtime.getRuntime();
+        var inizio = System.currentTimeMillis();
+        var res = mbase.mbase(matrix);
+        var fine = System.currentTimeMillis();
+        var memoryUsed = runtime.totalMemory() - runtime.freeMemory();
+        
+        System.out.printf("Le dimensioni dell'istanza in ingresso sono: %d x %d \n", matrix.length, matrix[0].length);
+        System.out.printf("I tempi di esecuzione dell'algoritmo per questa istanza sono: %d millisecondi \n", (fine-inizio));
+        System.out.printf("La memoria usata è: %d KB", memoryUsed/1024);
+           
     }
 }
