5fc6df41278f37f98ca7a84ff048b25db98920cd
Mic293 <mmichela2903@gmail.com>
2022-03-16 18:06:28 +0100

    Modified application only for MBase algorithm

--- src/main/Application.java
+++ src/main/Application.java
@@ -6,22 +6,40 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) throws FileNotFoundException {
-        
+
         var line = "C:\\Users\\mmich\\Desktop\\VS_workspace\\Algoritmi-2020-2021\\src\\main\\resources\\74L85.000.matrix";
-        
+
         var mbase = new MBase();
+        //var mvariant1 = new MVariant1();
         var matrix = MatrixParser.parse(line);
 
+        // Risultati con algoritmo Mbase
         var runtime = Runtime.getRuntime();
         var start = System.currentTimeMillis();
         var res = mbase.mbase(matrix);
         var end = System.currentTimeMillis();
         var memoryUsed = runtime.totalMemory() - runtime.freeMemory();
-        
-        System.out.printf("Le dimensioni dell'istanza in ingresso sono: %d x %d \n", matrix.length, matrix[0].length);
-        System.out.printf("I tempi di esecuzione dell'algoritmo per questa istanza sono: %d millisecondi \n", (end-start));
-        System.out.printf("La memoria usata è: %d KB \n", memoryUsed/1024);
+
+        // Risultati con algoritmo Mvariant1
+        /*var runtime1 = Runtime.getRuntime();
+        var start1 = System.currentTimeMillis();
+        var res1 = mvariant1.mvariant1(matrix);
+        var end1 = System.currentTimeMillis();
+        var memoryUsed1 = runtime1.totalMemory() - runtime1.freeMemory();*/
+
+        System.out.printf("Le dimensioni dell'istanza in ingresso sono: %d x %d \n\n", matrix.length, matrix[0].length);
+        System.out.println("------ MBASE ------");
+        System.out.printf("I tempi di esecuzione dell'algoritmo MBase per questa istanza sono: %d millisecondi \n",
+                (end - start));
+        System.out.printf("La memoria usata è: %d KB \n", memoryUsed / 1024);
         System.out.printf("Numero MHS trovati %d: \n", res.size());
         res.forEach(mhs -> System.out.println(Arrays.asList(mhs)));
+
+        /*System.out.println("------ MVariant1 ------");
+        System.out.printf("I tempi di esecuzione dell'algoritmo MVariant1 per questa istanza sono: %d millisecondi \n",
+                (end1 - start1));
+        System.out.printf("La memoria usata è: %d KB \n", memoryUsed1 / 1024);
+        System.out.printf("Numero MHS trovati %d: \n", res1.size());
+        res1.forEach(mhs -> System.out.println(Arrays.asList(mhs)));*/
     }
 }
