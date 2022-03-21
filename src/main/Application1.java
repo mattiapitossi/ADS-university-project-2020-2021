c146e61429f3648d04bc142c13691bd23acb2a59
Mic293 <mmichela2903@gmail.com>
2022-03-21 21:45:40 +0100

    Add Application for MBaseRefactor

--- src/main/Application1.java
+++ src/main/Application1.java
@@ -1,33 +1,29 @@
 
-import algorithms.MVariant1;
+import algorithms.MBaseRefactorprova1;
 import utils.MatrixParser;
 
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 
 public class Application1 {
     public static void main(String[] args) throws FileNotFoundException {
 
         var line = "C:\\Users\\mmich\\Desktop\\VS_workspace\\Algoritmi-2020-2021\\src\\main\\resources\\74L85.000.matrix";
 
-     
-        var mvariant1 = new MVariant1();
+        var mbaseRefactor = new MBaseRefactorprova1();
         var matrix = MatrixParser.parse(line);
 
-
-        // Risultati con algoritmo Mvariant1
+        // Risultati con algoritmo MBaseRefactor
         var runtime1 = Runtime.getRuntime();
         var start1 = System.currentTimeMillis();
-        var res1 = mvariant1.mvariant1(matrix);
+        var res1 = mbaseRefactor.mBaseRefactorprova1(matrix);
         var end1 = System.currentTimeMillis();
         var memoryUsed1 = runtime1.totalMemory() - runtime1.freeMemory();
 
-
-        System.out.println("------ MVariant1 ------");
+        System.out.println("------ MBaseRefactor ------");
         System.out.printf("I tempi di esecuzione dell'algoritmo MVariant1 per questa istanza sono: %d millisecondi \n",
                 (end1 - start1));
         System.out.printf("La memoria usata è: %d KB \n", memoryUsed1 / 1024);
         System.out.printf("Numero MHS trovati %d: \n", res1.size());
-        res1.forEach(mhs -> System.out.println(Arrays.asList(mhs)));
+        res1.forEach(mhs -> System.out.println(mhs));
     }
 }
