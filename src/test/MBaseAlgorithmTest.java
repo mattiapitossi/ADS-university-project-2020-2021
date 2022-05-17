4c65c09873815fbeda8f91504a4af9b443f7814f
michelafarruggia <michelafarruggia@icloud.com>
2022-05-17 19:03:11 +0200

    Update tests according to new expected output result

--- src/test/MBaseAlgorithmTest.java
+++ src/test/MBaseAlgorithmTest.java
@@ -24,9 +24,9 @@ public class MBaseAlgorithmTest {
 
         var myArrayList = new ArrayList<String[]>();
 
-        var mhs0 = new String[] { "c2", "c2", "c2", "c2", "c2", "c2", "c2" };
-        var mhs1 = new String[] { "c1", "x", "c1", "c5", "x", "x", "c3" };
-        var mhs2 = new String[] { "x", "x", "c1", "c0", "x", "x", "c3" };
+        var mhs0 = new String[] { null, null, "c2", null, null, null};
+        var mhs1 = new String[] { null, "c1", null, "c3", null, "c5"};
+        var mhs2 = new String[] { "c0", "c1", null, "c3", null, null};
         myArrayList.add(mhs0);
         myArrayList.add(mhs2);
         myArrayList.add(mhs1);
