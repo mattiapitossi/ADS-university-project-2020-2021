c3fb69077938097b8c589c4923256f29db77e442
MattiaPitossi <mattiapitossi@icloud.com>
2022-05-22 10:54:15 +0200

    Add test for remove rows function with pre-elaboration process

--- src/test/MBaseAlgorithmTest.java
+++ src/test/MBaseAlgorithmTest.java
@@ -1,9 +1,12 @@
 import algorithms.MBase;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import utils.MatrixPreElaboration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MBaseAlgorithmTest {
 
@@ -11,22 +14,22 @@ public class MBaseAlgorithmTest {
     void testMBaseAlgorithm() throws Exception {
         var mbase = new MBase();
         String[][] arr = {
-                { "c0", "c1", "c2", "0", "0", "0" },
-                { "0", "c1", "c2", "c3", "0", "c5" },
-                { "0", "c1", "c2", "0", "0", "0" },
-                { "c0", "0", "c2", "0", "0", "c5" },
-                { "c0", "c1", "c2", "c3", "0", "0" },
-                { "0", "c1", "c2", "c3", "0", "c5" },
-                { "0", "0", "c2", "c3", "0", "0" }
+                {"c0", "c1", "c2", "0", "0", "0"},
+                {"0", "c1", "c2", "c3", "0", "c5"},
+                {"0", "c1", "c2", "0", "0", "0"},
+                {"c0", "0", "c2", "0", "0", "c5"},
+                {"c0", "c1", "c2", "c3", "0", "0"},
+                {"0", "c1", "c2", "c3", "0", "c5"},
+                {"0", "0", "c2", "c3", "0", "0"}
         };
 
         var output = mbase.mbase(arr);
 
         var myArrayList = new ArrayList<String[]>();
 
-        var mhs0 = new String[] { null, null, "c2", null, null, null};
-        var mhs1 = new String[] { null, "c1", null, "c3", null, "c5"};
-        var mhs2 = new String[] { "c0", "c1", null, "c3", null, null};
+        var mhs0 = new String[]{null, null, "c2", null, null, null};
+        var mhs1 = new String[]{null, "c1", null, "c3", null, "c5"};
+        var mhs2 = new String[]{"c0", "c1", null, "c3", null, null};
         myArrayList.add(mhs0);
         myArrayList.add(mhs2);
         myArrayList.add(mhs1);
@@ -35,19 +38,33 @@ public class MBaseAlgorithmTest {
     }
 
     @Test
+    void testMBaseAlgorithmPre() throws Exception {
+        var mbase = new MatrixPreElaboration();
+        var row1 = Arrays.asList("c0","c1","c2");
+        var row2 = Arrays.asList("0","c1","c2");
+        var matrix = new ArrayList<List<String>>();
+        matrix.add(row1);
+        matrix.add(row2);
+
+        //we should be careful to not input an immutable list
+        var res = mbase.removeRows(matrix);
+    }
+
+    @Test
     void testMBaseAlgorithmMat1() throws Exception {
         var mbase = new MBase();
-        String[][] arr = { { "0", "0", "c2", "c3", "0", "0" },
-                { "0", "c1", "0", "c3", "0", "c5" }
+        String[][] arr = {
+                {"0", "0", "c2", "c3", "0", "0"},
+                {"0", "c1", "0", "c3", "0", "c5"}
         };
 
         var output = mbase.mbase(arr);
 
         var myArrayList = new ArrayList<String[]>();
 
-        var mhs0 = new String[] { "c3", "c3" };
-        var mhs1 = new String[] { "c2", "c1" };
-        var mhs2 = new String[] { "c2", "c5" };
+        var mhs0 = new String[]{"c3", "c3"};
+        var mhs1 = new String[]{"c2", "c1"};
+        var mhs2 = new String[]{"c2", "c5"};
         myArrayList.add(mhs0);
         myArrayList.add(mhs1);
         myArrayList.add(mhs2);
@@ -59,18 +76,18 @@ public class MBaseAlgorithmTest {
     void testMBaseAlgorithmMat2() throws Exception {
         var mbase = new MBase();
         String[][] arr = {
-                { "0", "c1", "c2", "0" },
-                { "c0", "c1", "c2", "c3" },
-                { "c0", "0", "c2", "c3" }
+                {"0", "c1", "c2", "0"},
+                {"c0", "c1", "c2", "c3"},
+                {"c0", "0", "c2", "c3"}
         };
 
         var output = mbase.mbase(arr);
 
         var myArrayList = new ArrayList<String[]>();
 
-        var mhs0 = new String[] { "c2", "c2", "c2" };
-        var mhs1 = new String[] { "c1", "x", "c0" };
-        var mhs2 = new String[] { "c1", "x", "c3" };
+        var mhs0 = new String[]{"c2", "c2", "c2"};
+        var mhs1 = new String[]{"c1", "x", "c0"};
+        var mhs2 = new String[]{"c1", "x", "c3"};
         myArrayList.add(mhs0);
         myArrayList.add(mhs1);
         myArrayList.add(mhs2);
