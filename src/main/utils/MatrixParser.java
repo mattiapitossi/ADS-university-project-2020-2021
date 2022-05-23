5b7248ad3ef5cfa031fdb24f471cfe52527e3932
Mattia <mattiapitossi@icloud.com>
2022-05-23 21:53:50 +0200

    Set processMatrix to from array to list of list

--- src/main/utils/MatrixParser.java
+++ src/main/utils/MatrixParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public final class MatrixParser {
@@ -13,7 +14,7 @@ public final class MatrixParser {
     private MatrixParser() {
     }
 
-    public static String[][] parse(String path) throws FileNotFoundException {
+    public static List<List<String>> parse(String path) throws FileNotFoundException {
         var sc = new Scanner(new BufferedReader(new FileReader(path)));
         var rowsCtx = new ArrayList<String>();
         var m = 0;
@@ -30,7 +31,7 @@ public final class MatrixParser {
         // DEBUG:
         // rowsCtx.stream().forEach(c -> System.out.println(c));
 
-        return processMatrix(rowsCtx, m);
+        return processMatrix2(rowsCtx, m);
     }
 
     private static String[][] processMatrix(ArrayList<String> ctx, int columns) {
@@ -46,6 +47,30 @@ public final class MatrixParser {
             }
         }
         return array;
+    }
+
+    private static List<List<String>> processMatrix2(ArrayList<String> ctx, int columns) {
+        String[][] array = new String[ctx.size()][columns];
+        for (var i = 0; i < array.length; i++) {
+            array[i] = ctx.get(i).split("\\s+");
+        }
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < array[0].length; j++) {
+                if (array[i][j].equals("1")) {
+                    array[i][j] = "c" + String.valueOf(j);
+                }
+            }
+        }
+        var list = new ArrayList<List<String>>();
+
+        for (var i = 0; i < array.length; i++) {
+            list.add(new ArrayList<>());
+            for (var j = 0; j < array[0].length; j++) {
+                list.get(i).add(array[i][j]);
+            }
+        }
+
+        return list;
     }
 
 }
