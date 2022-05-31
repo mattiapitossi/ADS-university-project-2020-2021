d21a9855ae21725c7347be0cba58d1a79533cce1
Mattia <mattiapitossi@icloud.com>
2022-05-31 22:56:47 +0200

    Improve tests

--- src/test/MBaseAlgorithmTest.java
+++ src/test/MBaseAlgorithmTest.java
@@ -12,15 +12,23 @@ public class MBaseAlgorithmTest {
     @Test
     void testMBaseAlgorithm() {
         var mbase = new MBase();
-        String[][] arr = {{"c0", "c1", "c2", "0", "0", "0"}, {"0", "c1", "c2", "c3", "0", "c5"}, {"0", "c1", "c2", "0", "0", "0"}, {"c0", "0", "c2", "0", "0", "c5"}, {"c0", "c1", "c2", "c3", "0", "0"}, {"0", "c1", "c2", "c3", "0", "c5"}, {"0", "0", "c2", "c3", "0", "0"}};
+        String[][] arr = {
+                {"c0", "c1", "c2", "0", "0", "0"},
+                {"0", "c1", "c2", "c3", "0", "c5"},
+                {"0", "c1", "c2", "0", "0", "0"},
+                {"c0", "0", "c2", "0", "0", "c5"},
+                {"c0", "c1", "c2", "c3", "0", "0"},
+                {"0", "c1", "c2", "c3", "0", "c5"},
+                {"0", "0", "c2", "c3", "0", "0"}
+        };
 
         var output = mbase.mbase(arr, "test", 8L);
 
         var myArrayList = new ArrayList<String[]>();
 
-        var mhs0 = new String[]{null, null, "c2", null, null, null};
-        var mhs1 = new String[]{null, "c1", null, "c3", null, "c5"};
-        var mhs2 = new String[]{"c0", "c1", null, "c3", null, null};
+        var mhs0 = new String[]{"c2"};
+        var mhs1 = new String[]{"c1", "c3","c5"};
+        var mhs2 = new String[]{"c0", "c1","c3"};
         myArrayList.add(mhs0);
         myArrayList.add(mhs2);
         myArrayList.add(mhs1);
